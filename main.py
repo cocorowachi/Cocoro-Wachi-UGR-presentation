8653ffff33e1979affdd88a74690c9904db66388
Cocoro Wachi <113720641+cocorowachi@users.noreply.github.com>
2026-02-16 21:49:37 -0600

    updated resume pdf

--- main.py
+++ main.py
@@ -332,7 +332,7 @@ if st.session_state.page == "authors":
             st.write("B.S. Computer Science Major, Mathematics Minor, UX Design Minor.")
             st.write("Dwight & Dian Diercks School of Advanced Computing.")
             st.write("+1(224)345-1255, wachic@msoe.edu")
-            with open("graphics/wachi_resume.pdf", "rb") as f:
+            with open("graphics/wachi.pdf", "rb") as f:
                 pdf_bytes = f.read()
             st.download_button("Download Resume", data=pdf_bytes, file_name="Cocoro Wachi Resume.pdf")
     st.write("---")
